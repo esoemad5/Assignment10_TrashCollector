cb516fe36ee43a2acdcf74e93fac7b05069f93c4
esoemad5 <esoemad5@gmail.com>
2018-10-30 10:10:22 -0500

    Roles implemented. Users see a different nav bar depending on their role (or lack thereof) Database gets automatically seeded with Customer and Employee roles on startup, if those roles do not already exist. Users do NOT get assigned to a role on registration.

--- TrashCollector/TrashCollector/Startup.cs
+++ TrashCollector/TrashCollector/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using System.Linq;
+using TrashCollector.Models;
 
 [assembly: OwinStartupAttribute(typeof(TrashCollector.Startup))]
 namespace TrashCollector
@@ -9,6 +13,34 @@ namespace TrashCollector
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRoles();
+        }
+
+        private void CreateRoles()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            if(db.Roles.ToList().Count == 0)
+            {
+                /* Could add roles here, but decided to do it the way Microsoft does
+                 * Not super comfortable with RoleManager, IdentityRole, and RoleStore classes but will use them anyway.
+                 * They fufill my needs and contain lots of potentially useful things that (hopewfully) allow me to adhere to O of SOLID better.
+                 */
+            }
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            if (!roleManager.RoleExists("Customer")/*roleManager.RoleExistsAsync("Customer").Result*/)
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
         }
     }
 }
