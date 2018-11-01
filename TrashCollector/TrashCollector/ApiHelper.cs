3c7620d0aa7de65f5e03389c4fc5f3e42a4760b5
esoemad5 <esoemad5@gmail.com>
2018-11-01 11:08:48 -0500

    Created static class ApiHelper. Preparing to store Customer's latitude and longitude.

---
+++ TrashCollector/TrashCollector/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace TrashCollector
+{
+    public static class ApiHelper
+    {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitilizeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
